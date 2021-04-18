9186d994b7c11c54344eb64e30bd2c2448799171
kamishic <kamishi6@gmail.com>
2021-04-18 12:34:58 +0900

    add getTodos CreateTodo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  
+  getTodos = async() => {
+    const getTodosUrl = 'http://localhost:8080'
+
+    await fetch(getTodosUrl)
+    .then(response => {
+      console.log(response);
+      response.json();
+    })
+    .then(data => console.log(data))
+    .catch((error) => {
+      console.log(error);
+      //console.error('Error:', error);
+    })
+  }  
+
+  createTodo = async() => {
+    const postTodoUrl = 'http://app-server-nest.herokuapp.com/todos/';
+    const postObject = {
+      name:'Taro',
+      todo:'myTask3'
+    };
+    console.log ( JSON.stringify(postObject));
+
+    await fetch(postTodoUrl,
+        {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(postObject)
+        }
+      )
+    .then(response => {
+      response.json();
+    })
+    .then(data => console.log(data))
+    .catch((error) => {
+      console.log(error);
+      //console.error('Error:', error);
+    })
+  }
 }
